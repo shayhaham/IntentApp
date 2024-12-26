c2112fd95b276ad48b698b6ca24fb0080e2fdc85
שי חכם <shayhaham956@gmail.com>
2024-12-26 17:44:12 +0200

    Two

--- app/src/main/java/haham/shay/intentapp/ShareActivity.java
+++ app/src/main/java/haham/shay/intentapp/ShareActivity.java
@@ -18,11 +18,11 @@ public class ShareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
-        EditText shareTextEditText = findViewById(R.id.shareEditText);
+        EditText shareTextEditText = findViewById(R.id.shareTextEditText);
         Button shareButton = findViewById(R.id.shareButton);
 
         shareButton.setOnClickListener(v ->{
-           String textToShare = shareEditText.getText().toString();
+           String textToShare = shareTextEditText.getText().toString();
             Intent shareIntent = new Intent();
             shareIntent.setAction(Intent.ACTION_SEND);
             shareIntent.putExtra(Intent.EXTRA_TEXT, textToShare);
