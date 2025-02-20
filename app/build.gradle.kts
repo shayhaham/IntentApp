14dfa0e0b459d8724fde4763ecb0e08f84b9f92c
שי חכם <shayhaham956@gmail.com>
2025-02-20 11:03:40 +0200

    Two

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation 'com.github.bumptech.glide:glide:4.16.0'
+    implementation 'com.google.android.gms:playservices-auth:20.7.0'
 }
